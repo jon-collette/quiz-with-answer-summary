40f3a8eab4076f9071fd8314bd7025ae1d6f63d2
Jon Collette <120140497+jon-collette@users.noreply.github.com>
2023-12-04 12:44:56 -0600

    init

--- script.js
+++ script.js
@@ -1,79 +1,143 @@
 console.clear();
-// helper functions
+// ==== Helper functions ====
+
+// Random num gen
 function rand(min, max) {
 	return Math.round(Math.random() * (max - min) + min);
 }
-var data = [{
-	shortname: "q1",
-	question: '<span class="eyebrow">Question 1</span><h2>I enjoy spending time outdoors.</h2><p>This question explores individual preferences towards outdoor activities and nature.</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.20, 0.10, 0.40, 0.30]
-}, {
-	shortname: "q2",
-	question: '<span class="eyebrow">Question 2</span><h2>I believe technology improves our quality of life.</h2><p>Assesses opinions on the impact of technology on daily living and well-being.</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.10, 0.20, 0.35, 0.35]
-}, {
-	shortname: "q3",
-	question: '<span class="eyebrow">Question 3</span><h2>I prefer reading books over watching movies.</h2><p>Compares preferences between reading and watching movies for entertainment.</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.30, 0.25, 0.20]
-}, {
-	shortname: "q4",
-	question: '<span class="eyebrow">Question 4</span><h2>I think it\'s important to eat healthy foods.</h2><p>Surveys attitudes towards the importance of maintaining a healthy diet.</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.15, 0.15, 0.30, 0.40]
-}, {
-	shortname: "q5",
-	question: '<span class="eyebrow">Question 5</span><h2>I feel comfortable meeting new people.</h2><p>Evaluates comfort levels in social interactions, particularly with new acquaintances.</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.30, 0.20, 0.25, 0.25]
-}];
 
-var $questionsContainer = $('#questions-container');
+// Function to help move the carousel by console
+function slideto(index) {
+	var slideto = index;
+	var totalSteps = $("#contentcarousel .carousel-item").length;
+	// basic error proofing
+	if (slideto > totalSteps) {
+		slideto = totalSteps-1;
+	} else if (slideto < 0) {
+		slideto = 0;
+	}
+	$("#contentcarousel").carousel(slideto);
+}
+
+// ==== The code ====
+// data input
+var data = [{
+    shortname: "q1",
+    eyebrow: "Question 1",
+    question: "I enjoy spending time outdoors",
+    description: "This question explores individual preferences towards outdoor activities and nature",
+    answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+    results: [0.20, 0.10, 0.40, 0.30]
+}, {
+    shortname: "q2",
+    eyebrow: "Question 2",
+    question: "I believe technology improves our quality of life",
+    description: "Assesses opinions on the impact of technology on daily living and well-being.",
+    answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+    results: [0.10, 0.20, 0.35, 0.35]
+}/*, {
+    shortname: "q3",
+    eyebrow: "Question 3",
+    question: "I prefer reading books over watching movies",
+    description: "Compares preferences between reading and watching movies for entertainment.",
+    answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+    results: [0.25, 0.30, 0.25, 0.20]
+}, {
+    shortname: "q4",
+    eyebrow: "Question 4",
+    question: "I think it's important to eat healthy foods",
+    description: "Surveys attitudes towards the importance of maintaining a healthy diet.",
+    answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+    results: [0.15, 0.15, 0.30, 0.40]
+}, {
+    shortname: "q5",
+    eyebrow: "Question 5",
+    question: "I feel comfortable meeting new people",
+    description: "Evaluates comfort levels in social interactions, particularly with new acquaintances.",
+    answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+    results: [0.30, 0.20, 0.25, 0.25]
+}*/];
+
+// Container
+var $questionsContainer = $("#questions-container");
 
 // Iterate through each item in the dataset
 $.each(data, function(index, item) {
 	// Create a new question block
-	var $questionBlock = $('<div class="carousel-item"></div>');
-	var $innerContainer = $('<div class="p-3"><div class="row"><div class="col"></div></div></div>');
+	var $questionBlock = $("<div class='carousel-item'></div>");
+	var $innerContainer = $("<div class='p-3'><div class='row'><div class='col'></div></div></div>");
 	$questionBlock.append($innerContainer);
-	var $colContainer = $innerContainer.find('.col');
-	// Insert the question text
-	var questionHtml = item.question; // This already contains the required HTML
-	$colContainer.append('<div class="text-block">' + questionHtml + '</div>');
+	var $colContainer = $innerContainer.find(".col");
+
+	// Insert the prompt text
+	var questionHtml = "";
+	if (item.eyebrow) {
+        questionHtml += "<span class='eyebrow'>" + item.eyebrow + "</span>";
+    }
+    if (item.question) {
+        questionHtml += "<h2>" + item.question + "</h2>";
+    }
+    if (item.description) {
+        questionHtml += "<p>" + item.description + "</p>";
+    }	
+	$colContainer.append("<div class='text-block'>" + questionHtml + "</div>");
+	
 	// Create answer options
-	var $answersRow = $('<div class="row mb-3"></div>');
+	var $answersRow = $("<div class='row mb-3'></div>");
 	$.each(item.answers, function(ansIndex, answer) {
-		var answerId = item.shortname + '-' + ansIndex;
-		var $answerCol = $('<div class="col-3"></div>');
-		var $formCheck = $('<div class="form-check"></div>');
-		$formCheck.append('<input class="form-check-input" id="' + answerId + '" name="' + item.shortname + '-selection" type="radio" value="' + answerId + '">');
-		$formCheck.append('<label class="form-check-label" for="' + answerId + '">' + answer + '</label>');
+		var answerId = item.shortname + "-" + ansIndex;
+		var $answerCol = $("<div class='col-lg-3'></div>");
+		var $formCheck = $("<div class='form-check'></div>");
+		$formCheck.append("<input class='form-check-input' id='" + answerId + "' name='" + item.shortname + "' type='radio' value='" + answerId + "'>");
+		$formCheck.append("<label class='form-check-label' for='" + answerId + "'>" + answer + "</label>");
 		$answerCol.append($formCheck);
 		$answersRow.append($answerCol);
 	});
 	$colContainer.append($answersRow);
 	// Append navigation buttons
-	var $buttonGroup = $('<div class="button-group text-center"></div>');
-	$buttonGroup.append('<button class="btn btn-primary" data-slide-to="prev">Previous question</button>');
-	$buttonGroup.append(' ');
-	$buttonGroup.append('<button class="btn btn-primary" data-slide-to="next">Next question</button>');
+	var $buttonGroup = $("<div class='button-group text-center'></div>");
+	$buttonGroup.append("<button class='btn btn-primary' data-slide-to='prev'>Previous question</button>");
+	$buttonGroup.append(" ");
+	$buttonGroup.append("<button class='btn btn-primary' data-slide-to='next'>Next question</button>");
 	$colContainer.append($buttonGroup);
 	// Append the entire question block to the container
 	$questionsContainer.append($questionBlock);
 });
-// move results to the end
-$('#results').appendTo($('#results').parent());
-// append step values to everything in the carousel
-$('#contentcarousel .carousel-item').each(function(index) {
-	$(this).attr('data-progress-step', index);
+
+var $resultsBlock = $("#results-block");
+$.each(data, function(index, item) {
+	// Create a new result block
+	var $resultBlock = $("<div id='result-" + item.shortname + "'></div>");
+	// Add the question header
+	$resultBlock.append("<code>" + item.question + ":</code>");
+	// Create the progress bars
+	var $progressDiv = $("<div class='progress my-3'></div>");
+	$.each(item.results, function(resultIndex, result) {
+		// Create each progress bar
+		var $progressBar = $("<div class='progress-bar results-bar result-" + item.shortname + "-a" + (resultIndex + 1) + "' role='progressbar' style='width: " + (result * 100) + "%;'></div>");
+		$progressDiv.append($progressBar);
+	});
+	// Append the progress bars to the result block
+	$resultBlock.append($progressDiv);
+	// Append the result block to the results-block container
+	$resultsBlock.append($resultBlock);
 });
+
+// append step values to everything in the carousel
+$("#contentcarousel .carousel-item").each(function(index) {
+	$(this).attr("data-progress-step", index);
+});
+
+// move results to the end
+$("#results").appendTo($("#results").parent());
+// move results to front for debug
+/* $("#results").prependTo($("#results").parent());
+slideto(-1); */
 
 // advanced slide control
 $("*[data-slide-to]").on("click", function() {
 	var slide = $(this).data("slide-to");
-	var currentIndex = $(this).closest(".carousel").find('.carousel-item.active').index();
+	var currentIndex = $(this).closest(".carousel").find(".carousel-item.active").index();
 	if (slide === "next") {
 		$(this).closest(".carousel").carousel("next");
 		updateProgressBar(currentIndex+1);
@@ -91,10 +155,24 @@ $("*[data-slide-to]").on("click", function() {
 	}
 });
 
-// Function to update the progress bar, snappier progress bar update when told on click where it's going
+// Attach change event listener to radio buttons
+$(".form-check-input").on("change", function() {
+    // Get the name of the group, which corresponds to the question shortname
+    var questionName = $(this).attr("name");
+    // Remove existing user-selected classes for this question
+    $("#result-" + questionName + " .user-selected").removeClass("user-selected");
+
+    // Get the index of the selected answer
+    var answerIndex = $(this).parent().parent().index();
+
+    // Add user-selected class to the corresponding progress bar in results
+    $("#result-" + questionName + " .results-bar").eq(answerIndex).addClass("user-selected");
+});
+
+// Function to update the progress bar, snappier progress bar update when told on click where it"s going
 function updateProgressBar(index) {
-	var totalSteps = $('#contentcarousel .carousel-item').length;
+	var totalSteps = $("#contentcarousel .carousel-item").length;
 	// Calculate so step 1 is width: 0%, step -1 is width: 100%
 	var progressWidth = (index / (totalSteps - 1)) * 100;
-	$('#progress-bar').css('width', progressWidth + '%');
+	$("#progress-bar").css("width", progressWidth + "%");
 }
