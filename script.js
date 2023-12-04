424328ea9e0564d4660ca005f817b1d9fcdcdb87
Jon Collette <120140497+jon-collette@users.noreply.github.com>
2023-12-04 10:28:17 -0600

    dummy data update, some code rewrites

--- script.js
+++ script.js
@@ -1,74 +1,75 @@
-var data = [{
-	shortname: "q1",
-	question: '<span class="eyebrow">Scenario 1</span><h2>Ipsum</h2><p>Lorem</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.25, 0.25, 0.25]
-}, {
-	shortname: "q2",
-	question: '<span class="eyebrow">Scenario 2</span><h2>Ipsum</h2><p>Lorem</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.25, 0.25, 0.25]
-}, {
-	shortname: "q3",
-	question: '<span class="eyebrow">Scenario 3</span><h2>Ipsum</h2><p>Lorem</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.25, 0.25, 0.25]
-}, {
-	shortname: "q4",
-	question: '<span class="eyebrow">Scenario 4</span><h2>Ipsum</h2><p>Lorem</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.25, 0.25, 0.25]
-}, {
-	shortname: "q5",
-	question: '<span class="eyebrow">Scenario 5</span><h2>Ipsum</h2><p>Lorem</p>',
-	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
-	results: [0.25, 0.25, 0.25, 0.25]
-}];
-
-// Assuming you have a div with an ID 'questions-container' where you want to append all questions
-    var $questionsContainer = $('#questions-container');
-
-    // Iterate through each item in the dataset
-    $.each(data, function(index, item) {
-        // Create a new question block
-        var $questionBlock = $('<div class="carousel-item"></div>');
-        var $innerContainer = $('<div class="p-3"><div class="row"><div class="col"></div></div></div>');
-        $questionBlock.append($innerContainer);
-        var $colContainer = $innerContainer.find('.col');
-
-        // Insert the question text
-        var questionHtml = item.question; // This already contains the required HTML
-        $colContainer.append('<div class="text-block">' + questionHtml + '</div>');
-
-        // Create answer options
-        var $answersRow = $('<div class="row mb-3"></div>');
-        $.each(item.answers, function(ansIndex, answer) {
-            var answerId = item.shortname + '-' + ansIndex;
-            var $answerCol = $('<div class="col-3"></div>');
-            var $formCheck = $('<div class="form-check"></div>');
-            $formCheck.append('<input class="form-check-input" id="' + answerId + '" name="' + item.shortname + '-selection" type="radio" value="' + answerId + '">');
-            $formCheck.append('<label class="form-check-label" for="' + answerId + '">' + answer + '</label>');
-            $answerCol.append($formCheck);
-            $answersRow.append($answerCol);
-        });
-        $colContainer.append($answersRow);
-
-        // Append navigation buttons
-        var $buttonGroup = $('<div class="button-group text-center"></div>');
-        $buttonGroup.append('<button class="btn btn-primary mr-auto" data-slide-to="prev">Previous <i class="fas fa-arrow-left"></i></button>');
-        $buttonGroup.append('<button class="btn btn-primary ml-auto" data-slide-to="next">Next <i class="fas fa-arrow-right"></i></button>');
-        $colContainer.append($buttonGroup);
-
-        // Append the entire question block to the container
-        $questionsContainer.append($questionBlock);
-    });
-    $('#results').appendTo($('#results').parent());
-
 console.clear();
 // helper functions
 function rand(min, max) {
 	return Math.round(Math.random() * (max - min) + min);
 }
+var data = [{
+	shortname: "q1",
+	question: '<span class="eyebrow">Question 1</span><h2>I enjoy spending time outdoors.</h2><p>This question explores individual preferences towards outdoor activities and nature.</p>',
+	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+	results: [0.20, 0.10, 0.40, 0.30]
+}, {
+	shortname: "q2",
+	question: '<span class="eyebrow">Question 2</span><h2>I believe technology improves our quality of life.</h2><p>Assesses opinions on the impact of technology on daily living and well-being.</p>',
+	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+	results: [0.10, 0.20, 0.35, 0.35]
+}, {
+	shortname: "q3",
+	question: '<span class="eyebrow">Question 3</span><h2>I prefer reading books over watching movies.</h2><p>Compares preferences between reading and watching movies for entertainment.</p>',
+	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+	results: [0.25, 0.30, 0.25, 0.20]
+}, {
+	shortname: "q4",
+	question: '<span class="eyebrow">Question 4</span><h2>I think it\'s important to eat healthy foods.</h2><p>Surveys attitudes towards the importance of maintaining a healthy diet.</p>',
+	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+	results: [0.15, 0.15, 0.30, 0.40]
+}, {
+	shortname: "q5",
+	question: '<span class="eyebrow">Question 5</span><h2>I feel comfortable meeting new people.</h2><p>Evaluates comfort levels in social interactions, particularly with new acquaintances.</p>',
+	answers: ["Strongly disagree", "Disagree", "Agree", "Strongly agree"],
+	results: [0.30, 0.20, 0.25, 0.25]
+}];
+
+var $questionsContainer = $('#questions-container');
+
+// Iterate through each item in the dataset
+$.each(data, function(index, item) {
+	// Create a new question block
+	var $questionBlock = $('<div class="carousel-item"></div>');
+	var $innerContainer = $('<div class="p-3"><div class="row"><div class="col"></div></div></div>');
+	$questionBlock.append($innerContainer);
+	var $colContainer = $innerContainer.find('.col');
+	// Insert the question text
+	var questionHtml = item.question; // This already contains the required HTML
+	$colContainer.append('<div class="text-block">' + questionHtml + '</div>');
+	// Create answer options
+	var $answersRow = $('<div class="row mb-3"></div>');
+	$.each(item.answers, function(ansIndex, answer) {
+		var answerId = item.shortname + '-' + ansIndex;
+		var $answerCol = $('<div class="col-3"></div>');
+		var $formCheck = $('<div class="form-check"></div>');
+		$formCheck.append('<input class="form-check-input" id="' + answerId + '" name="' + item.shortname + '-selection" type="radio" value="' + answerId + '">');
+		$formCheck.append('<label class="form-check-label" for="' + answerId + '">' + answer + '</label>');
+		$answerCol.append($formCheck);
+		$answersRow.append($answerCol);
+	});
+	$colContainer.append($answersRow);
+	// Append navigation buttons
+	var $buttonGroup = $('<div class="button-group text-center"></div>');
+	$buttonGroup.append('<button class="btn btn-primary mr-auto" data-slide-to="prev">Previous <i class="fas fa-arrow-left"></i></button>');
+	$buttonGroup.append(' ');
+	$buttonGroup.append('<button class="btn btn-primary ml-auto" data-slide-to="next">Next <i class="fas fa-arrow-right"></i></button>');
+	$colContainer.append($buttonGroup);
+	// Append the entire question block to the container
+	$questionsContainer.append($questionBlock);
+});
+// move results to the end
+$('#results').appendTo($('#results').parent());
+// append step values to everything in the carousel
+$('#contentcarousel .carousel-item').each(function(index) {
+	$(this).attr('data-progress-step', index);
+});
+
 // advanced slide control
 $("*[data-slide-to]").on("click", function() {
 	var slide = $(this).data("slide-to");
@@ -88,31 +89,18 @@ $("*[data-slide-to]").on("click", function() {
 	} else {
 		console.log("Slide " + slide + " is not found.");
 	}
-	updateProgressBar();
 });
 
-
-
-// append step progress
-$('#contentcarousel .carousel-item').each(function(index) {
-	$(this).attr('data-progress-step', index);
+$('#contentcarousel').on('slid.bs.carousel', function () {
+    updateProgressBar();
 });
-// do initial progress update
-updateProgressBar();
 
 // Function to update the progress bar
 function updateProgressBar() {
-	var totalSteps = $('#contentcarousel .carousel-item').length - 1;
-	console.log({totalSteps});
-	var currentStep = $('#contentcarousel .carousel-item.active').data('progress-step');
-	console.log({currentStep});
-	var progressWidth = (currentStep / totalSteps) * 100;
-	console.log({progressWidth});
+	var totalSteps = $('#contentcarousel .carousel-item').length;
+	// Get the index of the active item
+	var currentStep = $('#contentcarousel .carousel-item.active').index();
+	// Calculate so step 1 is width: 0%, step -1 is width: 100%
+	var progressWidth = (currentStep / (totalSteps - 1)) * 100;
 	$('#progress-bar').css('width', progressWidth + '%');
 }
-
-$("body").append(data);
-console.log(data);
-
-// Call the function to update the progress bar
-// This can be triggered based on your carousel's events
